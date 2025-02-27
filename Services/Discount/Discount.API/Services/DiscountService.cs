a2a4e4406189123e132aa7dff8e25e3f8c787c94
Emre ÖZGÖZ <emreozgoz@gmail.com>
2025-02-27 16:47:12 +0300

    discount module added

---
+++ Services/Discount/Discount.API/Services/DiscountService.cs
@@ -0,0 +1,59 @@
+﻿using Discount.Application.Commands;
+using Discount.Application.Queries;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.API.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IMediator _mediator;
+        public DiscountService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var query = new GetDiscountQuery(request.ProductName);
+            var result = await _mediator.Send(query);
+            return result;
+        }
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var query = new CreateDiscountCommand
+            {
+                ProductName = request.Coupon.ProductName,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+            };
+            var result = await _mediator.Send(query);
+            return result;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var query = new DeleteDiscountCommand(request.ProductName);
+
+            var deleted = await _mediator.Send(query);
+            var response = new DeleteDiscountResponse
+            {
+                Success = deleted
+            };
+            return response;
+        }
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var query = new UpdateDiscountCommand
+            {
+                Id = request.Coupon.Id,
+                ProductName = request.Coupon.ProductName,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+            };
+            var result = await _mediator.Send(query);
+            return result;
+        }
+    }
+}
